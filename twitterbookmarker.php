0aec00942ce1b7c8e18cc7c212ff64927b989f3e
Aditya Mukherjee <ditya@adityamukherjee.com>
2009-05-29 14:14:12 +0530

    verbose tags for bookmarks

--- twitterbookmarker.php
+++ twitterbookmarker.php
@@ -5,15 +5,21 @@
 	
 	$last_count = file_get_contents('lastFetch');
 	$tweets = json_decode(file_get_contents("http://USERNAME:PASSWORD@twitter.com/statuses/friends_timeline.json?since_id=$last_count&count=50"), true);
-	$id = $tweets[0]['id'];
-	$blacklist_users = array();
-	$blacklist_domains = array('twitpic', 'ow.ly', 'techcrunch', 'last.fm', 'jsmag');
+	if(!isset($tweets[0]))
+		die('No new tweets');
+	
+	file_put_contents('lastFetch', $tweets[0]['id']); # this script hates me
+	
+	$blacklist_users = array('ibnlive', 'mrinal', 'baxiabhishek', 'arjunghosh', 'ossguy', 'madguy000', 'thinkgeek', 'freddurst', 'hiway');
+	$blacklist_domains = array('twitpic', 'ow.ly', 'techcrunch', 'last.fm', 'jsmag', 'ibnlive');
+	$stopwords = explode(',', file_get_contents('stopwords'));
 	
 	foreach($tweets as $tweet):
 		if(in_array($tweet['user']['screen_name'], $blacklist_users))
 			continue;
 		
 		$s = $tweet['text'];
+		$dup = $s; # to generate tags later on
 		preg_match_all("/(?:http|https)\:\/\/(\S+)/", $s, $match);
 		if(count($match[0]) != 0): # make the call
 			
@@ -31,16 +37,19 @@
 
 			if(in_array($host, $blacklist_domains))
 				continue;
-				
-			echo $match[0][0] . "\n" . $url . "\n\n";
-
-			$delicious = file_get_contents("https://USERNAME:PASSWORD@api.del.icio.us/v1/posts/add?url=". urlencode($url) ."&description=" . urlencode($s) . "&tags=tweet-mark+" . $tweet['user']['screen_name']);
-			$return = simplexml_load_string($delicious);
-			if($return->attributes()->code != 'done')
-				file_put_contents('tweetMarksError', "$s\n" . $return->attributes()->code . "\n\n");
+			
+			foreach($stopwords as $word):
+				$dup = preg_replace("/\b$word\b/i", "", $dup);
+			endforeach;
+			
+			$dup = preg_replace("/[^\w\s]/", "", preg_replace("/(?:http|https)\:\/\/(\S+)/", "", $dup)); # one line baby!
+			$tags_string = strtolower(preg_replace(array("/\+{1,}/", "/\+$/"), array('+', ''), implode('+', explode(' ', $dup))));
+			
+			echo $match[0][0] . "\n" . $url . "\n" . $tags_string . "\n\n";
+			
+			$delicious = file_get_contents("https://USERNAME:PASSWORD@api.del.icio.us/v1/posts/add?url=". urlencode($url) ."&description=" . urlencode($s) . "&tags=tweet-mark+" . $tweet['user']['screen_name'] . "+" . $tags_string);
 		endif;
 	endforeach;
-	
-	file_put_contents('lastFetch', $id);
+
 	echo file_get_contents('tweetMarksError');
 ?>
